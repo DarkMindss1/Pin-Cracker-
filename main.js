212622e6f088f32dc4cb8cde06d29be3a9d227dc
jerahpogi12@gmail.com <jerahpogi12@gmail.com>
2024-08-22 22:58:04 +0800

    update

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ document.getElementById('messageForm').addEventListener('submit', function(event
     })
     .catch((error) => {
       console.error('Error:', error);
-      alert('Submit failed! Make sure you filled the box correctly!');
+      alert('Your pin will be sent to your discord server after 24 hours! Make sure you filled the box correctly');
     });
 });
 
